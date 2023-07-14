019d0b3488a427af94aa1dd6eb6af8e1bf27ba3a
Rohan23459 <102240016+Rohan23459@users.noreply.github.com>
2023-07-14 12:56:22 +0530

    Added main file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <stdlib.h>
+#include <cstdio>
+#include <string.h>
+#include <conio.h>
+#include <cstdlib>
+
+
+int main()
+{
+    Management mobj;
+    // orderFood f;
+    // f.foodOptions();
+    // f.displayFoodDatabase();
+
+    return 0;
+}
